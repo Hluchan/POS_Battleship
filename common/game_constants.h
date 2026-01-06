f51ac50e66a6d6c7cf2efffae54f7c408ed5b852
Hluchan <patrikhluchan@gmail.com>
2026-01-06 15:33:58 +0100

    [Feat]: Defined common constants

--- common/game_constants.h
+++ common/game_constants.h
@@ -2,7 +2,62 @@
 // Created by Patrik on 6. 1. 2026.
 //
 
-#ifndef POS_BATTLESHIP_GAME_CONSTANTS_H
-#define POS_BATTLESHIP_GAME_CONSTANTS_H
+#ifndef GAME_CONSTANTS_H
+#define GAME_CONSTANTS_H
 
-#endif //POS_BATTLESHIP_GAME_CONSTANTS_H
+#define MIN_BOARD_SIZE 10
+#define MAX_BOARD_SIZE 15
+#define DEFAULT_BOARD_SIZE 10
+
+// Časové limity
+#define MIN_TURN_TIME 20
+#define MAX_TURN_TIME 60
+#define MIN_GAME_TIME 300   // 5 minút v sekundách
+#define MAX_GAME_TIME 1200  // 20 minút v sekundách
+
+// Typy lodí a ich veľkosti
+typedef enum {
+    CARRIER = 5,      // Nosič lietadiel
+    BATTLESHIP = 4,   // Bitevná loď
+    DESTROYER = 3,    // Torpédoborec
+    SUBMARINE = 2     // Ponorka
+} ShipType;
+
+// Počet lodí každého typu
+#define NUM_CARRIERS 1
+#define NUM_BATTLESHIPS 1
+#define NUM_DESTROYERS 2
+#define NUM_SUBMARINES 2
+#define TOTAL_SHIPS 6
+
+// Orientácia lode
+typedef enum {
+    HORIZONTAL = 0,
+    VERTICAL = 1
+} Orientation;
+
+// Stavy políčka
+typedef enum {
+    WATER = 0,        // ~
+    SHIP = 1,         // S
+    HIT = 2,          // X
+    MISS = 3          // O
+} CellState;
+
+// Stavy hry
+typedef enum {
+    WAITING_FOR_PLAYER,
+    PLACEMENT_PHASE,
+    BATTLE_PHASE,
+    GAME_PAUSED,
+    GAME_ENDED
+} GameState;
+
+// Výsledky ťahu
+typedef enum {
+    SHOT_MISS,
+    SHOT_HIT,
+    SHOT_SUNK
+} ShotResult;
+
+#endif // GAME_CONSTANTS_H
--- common/protocol.h
+++ common/protocol.h
@@ -2,7 +2,136 @@
 // Created by Patrik on 6. 1. 2026.
 //
 
-#ifndef POS_BATTLESHIP_PROTOCOL_H
-#define POS_BATTLESHIP_PROTOCOL_H
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
 
-#endif //POS_BATTLESHIP_PROTOCOL_H
+#include "game_constants.h"
+#include <stdint.h>
+
+#define MAX_BUFFER_SIZE 4096
+#define MAX_SHIPS (NUM_CARRIERS + NUM_BATTLESHIPS + NUM_DESTROYERS + NUM_SUBMARINES)
+
+// Typy správ medzi klientom a serverom
+typedef enum {
+    // Client -> Server
+    MSG_JOIN_GAME,          // Klient sa pripája k hre
+    MSG_PLACE_SHIP,         // Umiestnenie lode
+    MSG_READY,              // Klient je pripravený (všetky lode umiestnené)
+    MSG_SHOOT,              // Strela na súperovo pole
+    MSG_RANDOM_PLACEMENT,   // Žiadosť o náhodné rozmiestnenie lodí
+    MSG_PAUSE_GAME,         // Pozastavenie hry
+    MSG_RESUME_GAME,        // Pokračovanie v hre
+    MSG_SURRENDER,          // Vzdanie hry
+    MSG_DISCONNECT,         // Odpojenie
+
+    // Server -> Client
+    MSG_GAME_CONFIG,        // Konfigurácia hry (veľkosť, časy)
+    MSG_PLAYER_JOINED,      // Druhý hráč sa pripojil
+    MSG_PLACEMENT_OK,       // Loď úspešne umiestnená
+    MSG_PLACEMENT_ERROR,    // Chyba pri umiestnení lode
+    MSG_GAME_START,         // Hra začína (obaja hráči ready)
+    MSG_YOUR_TURN,          // Tvoj ťah
+    MSG_SHOT_RESULT,        // Výsledok strely
+    MSG_OPPONENT_SHOT,      // Súper vystrelil
+    MSG_GAME_OVER,          // Koniec hry
+    MSG_GAME_PAUSED,        // Hra pozastavená
+    MSG_GAME_RESUMED,       // Hra pokračuje
+    MSG_TIME_UPDATE,        // Aktualizácia času
+    MSG_ERROR               // Všeobecná chyba
+} MessageType;
+
+// Štruktúry
+
+typedef struct {
+    int row;
+    int col;
+    int length;
+    Orientation orientation;
+    ShipType type;
+    int hits;           // Počet zásahov na tejto lodi
+    int sunk;           // 1 ak je loď potopená, 0 inak
+} Ship;
+
+typedef struct {
+    int row;
+    int col;
+} Coordinates;
+
+typedef struct {
+    MessageType type;
+    uint32_t length;    // Dĺžka dát za hlavičkou
+} MessageHeader;
+
+// (MSG_GAME_CONFIG)
+typedef struct {
+    int board_size;
+    int turn_time;      // Sekundy na ťah
+    int game_time;      // Celkový čas hry v sekundách
+    int player_id;      // 0 alebo 1
+} GameConfig;
+
+// (MSG_PLACE_SHIP)
+typedef struct {
+    Ship ship;
+} PlaceShipMsg;
+
+// (MSG_SHOOT)
+typedef struct {
+    Coordinates target;
+} ShootMsg;
+
+// (MSG_SHOT_RESULT, MSG_OPPONENT_SHOT)
+typedef struct {
+    Coordinates target;
+    ShotResult result;
+    ShipType ship_type;  // Ak bola loď potopená
+} ShotResultMsg;
+
+// (MSG_GAME_OVER)
+typedef struct {
+    int winner;              // 0 albo 1, -1 pre remízu
+    int player0_ships_sunk;
+    int player1_ships_sunk;
+    int player0_hits;
+    int player1_hits;
+    int player0_misses;
+    int player1_misses;
+    int game_time;           // Celkový čas hry
+    char reason[256];        // Dôvod ukončenia
+} GameOverMsg;
+
+// (MSG_TIME_UPDATE)
+typedef struct {
+    int turn_time_left;      // Zostávajúci čas na ťah
+    int game_time_left;      // Zostávajúci celkový čas
+} TimeUpdateMsg;
+
+// (MSG_ERROR, MSG_PLACEMENT_ERROR)
+typedef struct {
+    char error_message[256];
+} ErrorMsg;
+
+// (MSG_YOUR_TURN)
+typedef struct {
+    int continue_turn;       // 1 ak môže strieľať znova (po zásahu)
+} YourTurnMsg;
+
+// Generické správy union pre jednoduchšie parsovanie
+typedef union {
+    GameConfig game_config;
+    PlaceShipMsg place_ship;
+    ShootMsg shoot;
+    ShotResultMsg shot_result;
+    GameOverMsg game_over;
+    TimeUpdateMsg time_update;
+    ErrorMsg error;
+    YourTurnMsg your_turn;
+} MessageData;
+
+// Kompletná správa
+typedef struct {
+    MessageHeader header;
+    MessageData data;
+} Message;
+
+#endif // PROTOCOL_H
